de61c14c0329588ca1ebebfe7f22dd6fcac5bcf1
Daniel Vaughan <github@danielvaughan.com>
2023-05-28 09:32:29 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", helloWorldHandler)
+
+	fmt.Println("Server starting on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
